1f28056db1902dc323650e1bf0da795fb7780697
Tom Palmer <remlapmot@hotmail.com>
2024-06-11 15:29:16 +0100

    Amend ylim() calls

--- updates/BWMR_updated.R
+++ updates/BWMR_updated.R
@@ -149,7 +149,7 @@ BWMR <- function(gammahat, Gammahat, sigmaX, sigmaY) {
   )
   plot3 <- ggplot2::ggplot(data = df3, mapping = ggplot2::aes(x = factor(serial_number), y = weight, fill = weight)) + ggplot2::geom_bar(stat = 'identity', position = 'dodge') +
     ggplot2::labs(x = "observation No.", y = "weight", title = "Plot3: Posterior mean of weight of each observation") +
-    ylim(0, 1) +
+    ggplot2::ylim(0, 1) +
     theme(axis.text.x = element_text(size = 5))
   # scale_x_discrete(breaks = seq(10, N, 20)) +
   
