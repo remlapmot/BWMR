82371b6b5807f2904ce0c47a081ec516a3678c9a
Tom Palmer <remlapmot@hotmail.com>
2024-06-12 11:18:25 +0100

    Helpfile for dataset

---
+++ R/ExampleData.R
@@ -0,0 +1,14 @@
+#' Example Mendelian randomization genotype summary level dataset
+#'
+#' An example Mendelian randomization dataset.
+#'
+#' @format ## `who`
+#' A data frame with 76 rows and 5 columns:
+#' \describe{
+#'   \item{SNP}{SNP genotype}
+#'   \item{beta.exposure}{SNP-exposure estimates}
+#'   \item{beta.outcome}{SNP-outcome estimates}
+#'   \item{se.exposure}{Standard errors of the SNP-exposure estimates}
+#'   \item{se.outcome}{Standard errors of the SNP-outcome estimates}
+#' }
+"ExampleData"
